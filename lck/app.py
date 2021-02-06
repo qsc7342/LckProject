58cb44707ed1041eb577f07d11d34881576c0653
UmTaeho-Evaporation <nintendo7342@naver.com>
2021-02-06 21:55:32 +0900

    부트스트랩 추가

--- lck/app.py
+++ lck/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect ,url_for, flash, session
 from flask_cors import CORS
-import pymysql
 import time
 import requests
 from bs4 import BeautifulSoup
