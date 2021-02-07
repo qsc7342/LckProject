ef1ff075b061651b8158a90c934c3041028c9bdc
UmTaeho-Evaporation <nintendo7342@naver.com>
2021-02-07 19:45:57 +0900

    blueprint로 라우터 전환

--- lck/app.py
+++ lck/app.py
@@ -3,65 +3,23 @@ from flask_cors import CORS
 import time
 import requests
 from bs4 import BeautifulSoup
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
+import config
+
+# db = SQLAlchemy()
+# migrate = Migrate()
 DEBUG = True
-app = Flask(__name__)
-# enable CORS
-app.config.from_object(__name__)
-app.secret_key = 'some_secret'
-CORS(app)
-
-
-### Routes for customer part start ###
-
-@app.route('/')
-def MainPage():
-	return render_template('index.html')
-
-@app.route('/rank')
-def TeamRank():
-	b = 1
-	url = "https://namu.wiki/w/2021%20LoL%20Champions%20Korea%20Spring"
-
-	response = requests.get(url)
-	soup = BeautifulSoup(response.content, 'html.parser')
-	rank = soup.select('#app > div > div:nth-child(2) > article > div:nth-child(5) > div:nth-child(2) > div > div > div:nth-child(15) > div > table > tbody')
-	t = []
-	k = []
-	r = []
-	for x in rank:
-		tr_tag = x.find_all('tr')
-		for y in tr_tag:
-			td_tag = y.find_all('td')
-			for q in td_tag:
-				div_tag = q.find_all('div')
-				for s in div_tag:
-					digit = s.get_text()
-					t.append(digit)
-	six = 0
-	four = 3
-
-	for i in range(11,98):
-		if(six == 6):
-			four -= 1
-			if(four > 0):
-				continue
-			else:
-				r.append(k)
-				k = []
-				four = 3
-				six = 0
-		else:
-			six += 1
-			k.append(t[i])
-			if(i == 97):
-				r.append(k)
-	
-	for team in range(0, 10):
-		r[team].append("../static/image/team/" + r[team][0] + ".png")
-
-	return render_template('rank.html',data_list = r)
-
+def create_app():
+	app = Flask(__name__)
+	app.config.from_object(__name__)
+	app.secret_key = 'some_secret'
+	CORS(app)
+	from views import mainpage, teamrankpage
+	app.register_blueprint(mainpage.bp)
+	app.register_blueprint(teamrankpage.bp)
+	return app
 
 if __name__ == '__main__':
 	app.run()
---
+++ lck/views/teamrankpage.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, render_template
+import requests
+from bs4 import BeautifulSoup
+
+bp = Blueprint('teamrank', __name__, url_prefix='/rank')
+
+@bp.route('/')
+def TeamRank():
+	b = 1
+	url = "https://namu.wiki/w/2021%20LoL%20Champions%20Korea%20Spring"
+
+	response = requests.get(url)
+	soup = BeautifulSoup(response.content, 'html.parser')
+	rank = soup.select('#app > div > div:nth-child(2) > article > div:nth-child(5) > div:nth-child(2) > div > div > div:nth-child(15) > div > table > tbody')
+	t = []
+	k = []
+	r = []
+	for x in rank:
+		tr_tag = x.find_all('tr')
+		for y in tr_tag:
+			td_tag = y.find_all('td')
+			for q in td_tag:
+				div_tag = q.find_all('div')
+				for s in div_tag:
+					digit = s.get_text()
+					t.append(digit)
+	six = 0
+	four = 3
+
+	for i in range(11,98):
+		if(six == 6):
+			four -= 1
+			if(four > 0):
+				continue
+			else:
+				r.append(k)
+				k = []
+				four = 3
+				six = 0
+		else:
+			six += 1
+			k.append(t[i])
+			if(i == 97):
+				r.append(k)
+	
+	for team in range(0, 10):
+		r[team].append("../static/image/team/" + r[team][0] + ".png")
+
+	return render_template('rank.html',data_list = r)
